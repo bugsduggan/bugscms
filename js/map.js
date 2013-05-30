5c45ea2dbd4565d0daed05ad3fa2c5d6ba09ce1e
Tom Leaman <tom@tomleaman.co.uk>
2013-05-30 23:56:20 +0100

    Get map transitions working

--- js/map.js
+++ js/map.js
@@ -1,6 +1,7 @@
 google.maps.visualRefresh = true;
 
 var map;
+var bounds;
 function initialize() {
 	console.log('initializing map');
 
@@ -11,20 +12,32 @@ function initialize() {
 		disableDefaultUI: true,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
-	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-	var marker = new google.maps.Marker({
-		position: center,
-		map: map
-	});
+	bounds = new google.maps.LatLngBounds();
+	for (i = 0; i < locations.length; i++) {
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(
+									locations[i]['lat'], locations[i]['lng']),
+			map: map
+		});
+		bounds.extend(marker.position);
+	}
+	map.fitBounds(bounds);
 }
 
 function mouse_over(id) {
 	console.log('Displaying event map for event '+id);
+	locations.forEach(function(loc) {
+		if (id == loc['id']) {
+			map.panTo(new google.maps.LatLng(loc['lat'], loc['lng']));
+		}
+	});
 }
 
 function mouse_out(id) {
 	console.log('Displaying main event map instead of event '+id);
+	map.panTo(bounds.getCenter());
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
